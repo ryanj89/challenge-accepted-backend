72475d50f5089329f6e5a041d815fffae2aa9585
ryanj89 <ryanj89@gmail.com>
2017-06-13 00:13:04 -0600

    CORS

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ const app = express();
 app.set('port', PORT);
 app.set('env', ENV);
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //  Middleware
 app.use(helmet());
