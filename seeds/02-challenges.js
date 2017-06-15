45461713d9e2b51f2a5757f3b12d55298ad334e7
ryanj89 <ryanj89@gmail.com>
2017-06-15 16:55:42 -0600

    added seed data

--- seeds/02-challenges.js
+++ seeds/02-challenges.js
@@ -3,6 +3,24 @@ exports.seed = knex => {
     .then(() => {
       return knex('challenges').insert([
         {
+          name: 'Go on a hike in the Rocky Mountains',
+          description: 'Go on a hike in the Rocky Mountains. Try to get a better view than this!',
+          creator_id: 2,
+          challenge_img: 'https://res.cloudinary.com/ryanj89/image/upload/v1497425990/french-italian-alps-walking-hiking-tour_vxrrwv.jpg',
+          category: 'outdoors',
+          private: false,
+          expires_at: new Date(2017, 6, 15)
+        },
+        {
+          name: 'Go on a bike ride through downtown',
+          description: 'Ride through downtown on a bike, how fast can you go?',
+          creator_id: 2,
+          challenge_img: 'https://http://res.cloudinary.com/ryanj89/image/upload/v1497567236/Bike-Lake_z1yf5e.jpg',
+          category: 'outdoors',
+          private: false,
+          expires_at: new Date(2017, 6, 15)
+        },
+        {
           name: 'Ryan\'s Private Challenge',
           description: 'This is a description for a challenge. It should describe what the challenge is and any requirements to fulfill the challenge',
           creator_id: 1,
@@ -19,15 +37,6 @@ exports.seed = knex => {
           category: 'music',
           private: false,
           expires_at: new Date(2017, 6, 10)
-        },
-        {
-          name: 'Go on a hike in the Rocky Mountains',
-          description: 'Go on a hike in the Rocky Mountains. Try to get a better view than this!',
-          creator_id: 2,
-          challenge_img: 'https://res.cloudinary.com/ryanj89/image/upload/v1497425990/french-italian-alps-walking-hiking-tour_vxrrwv.jpg',
-          category: 'outdoors',
-          private: false,
-          expires_at: new Date(2017, 6, 15)
         },
         {
           name: 'Lindsay\'s Private Challenge',
